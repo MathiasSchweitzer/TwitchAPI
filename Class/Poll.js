39abbcfbb5e25158131b77fb0c4d38761a5d3861
MathiasSchweitzer <mathias.schweitzer.travail@gmail.com>
2024-01-13 19:10:09 +0100

    Rajout Poll.js

---
+++ Class/Poll.js
@@ -0,0 +1,12 @@
+class Poll {
+    /**
+     * Create a poll instance
+     * @param {String} broadcaster_id User access token of the channel where post poll
+     * @param {String} title Poll's title
+     * @param {{title: String}[]} choices Choices list
+     * @param {*} duration 
+     * @param {*} channel_points 
+     */
+    constructor(broadcaster_id, title, choices, duration, channel_points) {
+    }
+}
